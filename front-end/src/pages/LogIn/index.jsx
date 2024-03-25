1b4f98a5f00282cd40fea38d9c1548c399f51eec
catarinacci <catarinacci@gmail.com>
2024-03-24 21:07:27 -0300

    install front-end

---
+++ front-end/src/pages/LogIn/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function LogIn(){
+    return(
+        <>
+            <h1>LogIn</h1>
+        </>
+    )
+}
---
+++ front-end/src/pages/SinglePost/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function SinglePost(){
+    return(
+        <>
+            <h1>SinglePost</h1>
+        </>
+    )
+}
